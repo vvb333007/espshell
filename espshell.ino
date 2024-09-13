8c30f480bad9421d3a82ba3c9de669dca6950d56
vvb333007 <vvb@nym.hush.com>
2024-09-13 21:14:27 +0700

    header & function references removed

--- espshell.ino
+++ espshell.ino
@@ -1,30 +1,23 @@
 #include <Arduino.h>
-//#include "espshell.h"
 
-#define LED 2 // Small blue LED on many ESP32 Devkit clones, 
+// Small blue LED on many ESP32 Devkit clones, 
+#define LED 2 
 
+// setup our serial port & led pin
 void setup() {
   
   Serial.begin(115200);
   delay(1000);
 
-  
-
   // prepare our blink led, pin2
   pinMode(LED,OUTPUT);
-    
-  // start shell task
-  //espshell_task("esp32#>");
 }
 
 
-
-
-
+// blink led to indicate running loop() task
+// type "suspend" in serial monitor to stop this task, type "resume" to resume
 void loop() {
 
-  // blink led to indicate running loop() task
-  // type "suspend" in serial monitor to stop this task, type "resume" to resume
   while(1) {
     digitalWrite(LED,0);
     delay(250);
