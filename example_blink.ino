fd591f847127f5d74f668430ae2068e1a8e91636
vvb333007 <vvb@nym.hush.com>
2024-10-08 17:47:11 +0700

    convar_add() example added

--- example_blink.ino
+++ example_blink.ino
@@ -3,6 +3,21 @@
 
 #include <Arduino.h>
 
+
+#if 0 // Set to 1 to see how sketch variables are accessed from the shell
+#include "extra/espshell.h"
+#else
+#define convar_add(X)
+#endif
+
+#define LED 2
+
+static unsigned int test = 0xdeadbeef;
+static unsigned char Char = 12;
+signed short Short = -1;
+
+
+
 // Small blue LED on many ESP32 Devkit clones, 
 #define LED 2 // ESPCam has led on pins 33 and 4
 
@@ -14,6 +29,12 @@ void setup() {
 
   // prepare our blink led, pin2
   pinMode(LED,OUTPUT);
+
+  //Make 3 variables accessible from the shell
+  convar_add(test);
+  convar_add(Char);
+  convar_add(Short);
+  
 }
 
 
