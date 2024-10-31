8dc89f52597a3a058597fe1f8b1a5a8fdba2e72a
vvb333007 <vvb@nym.hush.com>
2024-10-31 13:11:32 +0700

    Update espshell.h

--- extra/espshell.h
+++ extra/espshell.h
@@ -1,32 +1,14 @@
 #ifndef espshell_h
 #define espshell_h
 
-// -- ESPShell compile-time setting default values. --
-//
-// Uncomment whatever you need and change default value to values you want to
-// override default settings.
-//
-// IMPORTANT: also please #undef the setting BEFORE #define'ing it or it will
-//            be bunch of GCC warnings on macro redefinition.
-//            i.e. #undef POMPT before uncommenting #define PROMPT
-//
 
+// Settings & their default values.
+// If you choose to uncomment and change any of these
+// please add #undef before it or GCC will produce "redefinition" warnings
 
 //#define AUTOSTART 1                 // Autostart ESPShell with sketch.
-                                      // if disabled, then sketch can call "extern "C" void espshell_start();" function to start
-
-//#define PROMPT      "esp32#>"       // Normal prompt
-//#define PROMPT_I2C  "esp32-i2c#>"   // i2c prompt
-//#define PROMPT_UART "esp32-uart#>"  // uart prompt
-//#define PROMPT_SEQ  "esp32-seq#>"   // sequence prompt
-//#define PROMPT_VFAT "esp32-fat#>"   // FAT filesystem prompt
-
-//#define SERIAL_IS_USB               // class Serial is USBCDC (Arduino Nano ESP32 and othe USB-OTG enabled boards)
 //#define STARTUP_PORT UART_NUM_0     // Uart number (or 99 for USBCDC) where shell will be deployed at startup
-//#define DO_ECHO 1                   // -1 - espshell is completely silent, commands are executed but all screen output is disabled
-                                      //  0 - espshell does not do echo for user input (as modem ATE0 command). commands are executed and their output is displayed
-                                      //  1 - espshell default behaviour (do echo, enable all output)
-
+//#define DO_ECHO 1                   // -1,0,1
 //#define ESPCAM                      // Include ESP32CAM commands (read extra/README.md) or not.
 //#define WITH_COLOR 1                // Enable(1) / Disable(0) terminal colors
 //#define WITH_HELP 1                 // Set to 0 to save some program space by excluding help strings/functions
@@ -35,30 +17,17 @@
 //#define STACKSIZE 5000              // Shell task stack size
 //#define BREAK_KEY 3                 // Keycode of an "Exit" key: CTRL+C to exit uart "tap" mode
 //#define SEQUENCES_NUM 10            // Max number of sequences available for command "sequence"
+//#define WITH_FS  1                  // Filesystem support
+//#define WITH_FAT 1
+//#define WITH_LITTLEFS 1
+//#define WITH_SPIFFS 1
 
 
-
-
-// Register global or static variable to be accessible from espshell:
-// ----------------- Example Sketch Code -------------------
-// int my_var;
+// Register global or static variable to be accessible from espshell
 //
-// void setup() {
-//   ...
-//   espshell_var(my_var);
-// }
-// ---------------------------------------------------------
-//
-// Registered variables can be accessed by "var" command:
-//
-// esp32#>var my_var 666       <--- set variable
-// esp32#>var my_var           <--- display variable
-// esp32#>var                  <--- display list of registered variables
-#if 1
-#  define convar_add( VAR ) espshell_varadd( #VAR, &VAR, sizeof(VAR))
-#else
-#  define convar_add( VAR )
-#endif
+#define convar_add( VAR ) espshell_varadd( #VAR, &VAR, sizeof(VAR))
+//#define convar_add( VAR )
+
 
 
 
@@ -71,9 +40,12 @@ extern "C" {
 #endif
 
 
+// By default espshell shell autostarts. If AUTOSTART is set to 0 in espshell.h then
+// user sketch must call espshell_start() to manually start the shell.
 //
-
-
+#if !AUTOSTART
+void espshell_start();
+#endif
 
 // Execute an arbitrary shell command (\n are allowed for multiline).
 // it is an async call: it returns immediately. one can use espshell_exec_finished()
@@ -88,6 +60,7 @@ void espshell_exec(const char *p);
 // check if last espshell_exec() call has completed its
 // execution. Does NOT mean command itself finished its execution tho. This function
 // only tells if it is possible to enqueue more commands with espshell_exec() or not
+//
 bool espshell_exec_finished();
 
 
@@ -100,6 +73,7 @@ void espshell_varadd(const char *name, void *ptr, int size);
 // 
 // Special value 99 means USB console port for boards with USB-OTG
 // support
+//
 int console_attach2port(int i);
 
 
