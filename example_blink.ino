f192fde091f91e73adc785dcddc65eb04eb81655
vvb333007 <vvb@nym.hush.com>
2024-11-04 18:50:31 +0700

    comments & hints added

--- example_blink.ino
+++ example_blink.ino
@@ -1,9 +1,15 @@
 // Minimal sketch - example, blinking LED.
-// Also registers few variables to play with shell command "var"
+//
+// 1. Use command "suspend" or press Ctrl+Z to suspend sketch execution
+// 2. Use command "var" to display/change sketch variables (there are few)
+//    Note that setting variable "Blink" to "0" will cause blink sketch to
+//    end
+// 3. 
 
 #include <Arduino.h>
 
-
+// Include "espshell.h" if we have it to register variables
+// no header file? no fun with "var" command
 #if __has_include("espshell.h")
 #  include "espshell.h"
 #elif __has_include("extra/espshell.h")
@@ -12,19 +18,24 @@
 #  define convar_add(X)
 #endif
 
+// Choose your LED pin:
 #define LED 2       // Generic ESP32 Dev Board
-//#define LED 4     // ESP32Cam
-//#define LED 33    // ESP32Cam
+//#define LED 4     // ESP32Cam high intensity lamp
+//#define LED 33    // ESP32Cam low power red LED
 
 
-static unsigned int test = 0x1234;  // test variables for "var" shell command
-static unsigned char Char = 12;     // test variables for "var" shell command 
-       signed short Short = -1;     // test variables for "var" shell command
-static  int Blink = 1;              // "var Blink 0" will set Blink to 0 causing loop() to stop
+// Test variables for "var" shell command. They do nothing
+// but you can change or display them via "var" commands
+static unsigned int test = 0x1234;  
+static unsigned char Char = 12;
+       signed short Short = -1;
 
+// One more variable. Setting it to 0 via "var Blink 0" stops
+// this sketch
+static  int Blink = 1;
 
-
-// setup our serial port & led pin
+///////////////////////////////////
+// Setup our serial port & LED pin
 void setup() {
   
   Serial.begin(115200);
@@ -40,19 +51,18 @@ void setup() {
   
 }
 
-
+// Main loop
 void loop() {
 
-  Serial.println("Start blinking");
-  
+  Serial.println("\r\nStart blinking. Use \"var Blink 0\" to stop\r\n");
+
   while(Blink) {
-    digitalWrite(LED,0);
-    delay(250);
-    digitalWrite(LED,1);
-    delay(250);
+    digitalWrite(LED, LOW);  delay(250);
+    digitalWrite(LED, HIGH); delay(250);
   }
 
-  Serial.println("Stop blinking");
-
-  while (1) delay(9999);
+  Serial.println("\r\nStop blinking\r\n");
+  delay(9999);
+  Blink = 1;
+  Serial.println("Restarting..\r\n");
 } 
