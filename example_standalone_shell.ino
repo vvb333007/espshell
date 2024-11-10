04b902429dd414fa377a71c12658781c7526d786
vvb333007 <vvb@nym.hush.com>
2024-11-10 18:52:35 +0700

    Standalone shell

---
+++ example_standalone_shell.ino
@@ -0,0 +1,8 @@
+// Minimal sketch - standalone shell. 
+// Yes, as simple as that :)
+
+#include <Arduino.h>
+
+// Initialize Serial interface and let ESPShell do its job.
+void setup() { Serial.begin(115200); }
+void loop() { delay(9999); }
