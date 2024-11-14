94b4a18fd3c4d26211cadad07dbb1db457556e5d
vvb333007 <vvb@nym.hush.com>
2024-11-14 15:51:24 +0700

    Update espshell.h

--- extra/espshell.h
+++ extra/espshell.h
@@ -2,120 +2,130 @@
 #define espshell_h
 
 
-// -- ESPShell compile-time settings & their default values --
-//
-// If you choose to uncomment and change any of these please add #undef before it 
-// or GCC will produce "redefinition" warnings
-//
-// Example: disable filesystem support in ESPShell
-//
-// #undef WITH_FS
-// #define WITH_FS 0
-
-
-//#define ESPCAM                     // Add support for AiThinker ESP32-CAM (read extra/README.md).
-//#define AUTOSTART       1          // Set to 0 for manual shell start via espshell_start().
-//#define WITH_COLOR      1          // Add support for terminal colors
-//#define WITH_HELP       1          // Set to 0 to save some program space by excluding help strings/functions
-//#define WITH_HISTORY    1          // Keep it 1. Disable/enable history in runtime with hidden command "history off" or "history on"
-//#define WITH_FS         1          // Filesystems support
-//#define WITH_SPIFFS     1          //   add support for SPIF filesystem
-//#define WITH_LITTLEFS   1          //          --       LittleFS
-//#define WITH_FAT        1          //          --       FAT
-//#define HIST_SIZE       20         // History buffer size (number of commands to remember)
-//#define STARTUP_PORT    UART_NUM_0 // Uart number (or 99 for USBCDC) where shell will be deployed at startup.
-//#define SEQUENCES_NUM   10         // Max number of sequences available for command "sequence"
-//#define MOUNTPOINTS_NUM 5          // Max number of simultaneously mounted filesystems
-//#define STACKSIZE       (5*1024)   // Shell task stack size
-//#define DIR_RECURSION_DEPTH 127    // Max subdirectory depth: max path of 256 symbols can include 127 directories: "/a/a/a/a/a/a/a.../a"
-//#define MEMTEST         0          // Hunt for espshell's memory leaks. For ESPShell developers
-//#define DO_ECHO         1          // Echo mode at espshell startup: 1:normal, 0:don't do echo, -1:don't do any screen output (see "echo" command)
-
-
-// -- Access sketch variables from ESPShell --
-//
-// Yes it is possible, you just need to /register/ your variable by using
-// convar_add() macro. Once regitered, variables are available for read/write 
-// access via "var" command
+// -- ESPShell compile-time settings begin --
 // 
-// Example: register two sketch variables in ESPShell
-// ...
-// int some_variable;
-// static float volatile another_variable;
-// ...
-// convar_add(some_variable);
-// convar_add(another_variable);
+
+// ===> Disable or enable certain features. I bet you don't need all 3 filesystems support
 //
+//#define ESPCAM          // Add support for AiThinker ESP32-CAM (read extra/README.md).
+//undef AUTOSTART         // Disable autostart. User sketch can start the shell later by calling espshell_start()
+//#undef WITH_COLOR       // Disable ANSI terminal colors
+//#undef WITH_HELP        // Disable help system & hint messages to save program space
+//#undef WITH_FS          // Disable file manager completely, or ..
+//#undef WITH_SPIFFS      //     .. disable support for SPIFFS
+//#undef  WITH_LITTLEFS   //     .. disable support for LittleFS
+//#undef WITH_FAT         //     .. disable support for FAT/exFAT
+//#define MEMTEST 1       // Include code for memory-leaks analisys: espshell tracks its own memory allocations
+
+// ===> Runtime settings. Uncomment 2 lines at once to avoid GCC 'macro redefinition' warnings.
+// These settings are about various limits and startup
+
+// 1) UART port number (or 99 for USBCDC) where shell will be deployed at startup. Valid values are: UART_NUM_0, UART_NUM_1, UART_NUM_2 and 99
+//#undef STARTUP_PORT
+//#define STARTUP_PORT UART_NUM_0 
+
+// 2) Echo mode at espshell startup: 1:normal, 0:don't do echo, -1:don't do any screen output (see "echo" command)
+//#undef STARTUP_ECHO
+//#define STARTUP_ECHO 1          
+
+// 3) Max number of sequences that can be configured by command "sequence". Decrease it to save some RAM (min value is 1)
+//#undef SEQUENCES_NUM
+//#define SEQUENCES_NUM   10      
+
+// 4) Max number of simultaneously mounted filesystems
+//#undef MOUNTPOINTS_NUM
+//#define MOUNTPOINTS_NUM 5       
+
+// 5) Shell task stack size. 
+//#undef  STACKSIZE
+//#define STACKSIZE (5*1024)      
+
+// 6) Max subdirectory depth: path of maximum length of 256 symbols can hold 127 directories: "/a/a/a/a/a/a/a.../a"
+//    Bigger the number larger STACKSIZE is required
+//#undef DIR_RECURSION_DEPTH
+//#define DIR_RECURSION_DEPTH 127 
+
+// -- ESPShell compile-time settings end --
+
+// -- ESPShell public API --
+// 1) Access sketch variables from ESPShell while sketch is running
+//    Yes it is possible, you just need to /register/ your variable by using
+//    "convar_add(VAR)"" macro. Once registered, variables are available for 
+//    read/write access (via "var" command). Variable types supported: pointers,
+//    integers and floating point types both signed and unsigned
+// 
+//    Example: register two sketch variables in ESPShell
+//    ...
+//    int some_variable;
+//    const char *ptr;
+//    static float volatile another_variable;
+//    ...
+//    convar_add(ptr);
+//    convar_add(some_variable);
+//    convar_add(another_variable);
 #if 1
    extern float dummy_float;
-#  define convar_add( VAR ) espshell_varadd( #VAR, &VAR, sizeof(VAR), (__builtin_classify_type(VAR) == __builtin_classify_type(dummy_float)))
+   extern void *dummy_pointer;
+
+#  define convar_add( VAR ) \
+          espshell_varadd( #VAR, &VAR, sizeof(VAR), \
+          (__builtin_classify_type(VAR) == __builtin_classify_type(dummy_float)), \
+          (__builtin_classify_type(VAR) == __builtin_classify_type(dummy_pointer)))
 #else
 #  define convar_add( VAR ) do {} while( 0 )
 #endif
 
 
-
-//
-//  -- ESPShell API & utility functions --
-// These are for sketch-to-ESPShell interaction: changing the UART espshell is currently uses,
-// manually start espshell, executing arbitrary commands and so on
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
-// Start ESPShell
+// 2) Start ESPShell manually.
 // By default espshell shell autostarts. If AUTOSTART is set to 0 in espshell.h then
-// user sketch must call espshell_start() to manually start the shell.
+// user sketch must call espshell_start() to manually start the shell. Regardless of AUTOSTART:
+// a shell which was closed by "exit ex" command it is ok to call this function to restart the shell
 //
 #if !AUTOSTART
 void espshell_start();
 #endif
 
-// Execute an arbitrary shell command (\n are allowed for multiline).
-// it is an async call: it returns immediately. one can use espshell_exec_finished()
-// to check if actual shell command has finished its execution. Data pointed by "p"
-// must remain allocated until command execution finishes
+// 3) Execute an arbitrary shell command (\n are allowed for multiline).
+// This function injects its argument to espshell's input stream as if it was typed by user. 
+// It is an asyn call, returns immediately. Next call can be done only after espshell_exec_finished() 
+// returns /true/
 //
 // Example: espshell_exec("uptime \n cpu \n");
 //
 void espshell_exec(const char *p);
 
 
-// check if last espshell_exec() call has completed its
-// execution. Does NOT mean command itself finished its execution tho. This function
-// only tells if it is possible to enqueue more commands with espshell_exec() or not
+// 4) Check if ESPShell has finished processing of last espshell_exec() call and is ready for new espshell_exec()
+// This function does NOT tell you that command execution is finished. It is about readiness of the shell to accept new
+// commands
 //
 bool espshell_exec_finished();
 
 
-// Register sketch variable
 // DONT USE THIS! use convar_add() instead
-// /name/ - variable name
-// /ptr/  - pointer to variable
-// /size/ - 1,2 or 4: variable size in bytes
-// /isf/  - must be /true/ if registering a floating point variable
-//
-void espshell_varadd(const char *name, void *ptr, int size, bool isf);
+void espshell_varadd(const char *name, void *ptr, int size, bool isf, bool isp);
 
-// By default ESPShell occupies UART0. It could be changed
-// at compile time by setting #define USE_UART in espshell.h
+// 5) By default ESPShell occupies UART0. Default port could be changed
+// at compile time by setting #define STARTUP_PORT in "extra/espshell.h"
 // to required value OR at runtime by calling console_attach2port()
 // 
 // Special value 99 means USB console port for boards with USB-OTG
 // support
-//
+// In:
 // /port/ - 0, 1 or 2 for UART interfaces or 99 for native USB console 
 //          interface. If /port/ is negative number then this function 
 //          returns port number which currently in use by ESPShell.
-// 
+// Out:
 // Number of port which is now used
 //
 int console_attach2port(int port);
 
 
-// classic digitalRead() undergoes PeriMan checks (see Arduino Core, esp32-periman.c) 
+// 6) classic digitalRead() undergoes PeriMan checks (see Arduino Core, esp32-periman.c) 
 // which do not allow to read data for pins which are NOT configured as GPIO:  
 // UART pins or I2C pins are examples of pins that can not be read by digitalRead().
 //
@@ -126,7 +136,7 @@ int console_attach2port(int port);
 //
 int digitalForceRead(int pin);
 
-// Again, periferial manager bypassed: faster version
+// 7) Again, periferial manager bypassed: faster version
 // of digitalWrite():
 //
 // 1. pin not need to be set for OUTPUT
@@ -134,7 +144,7 @@ int digitalForceRead(int pin);
 //
 void digitalForceWrite(int pin, unsigned char level);
 
-// Discussion: https://github.com/espressif/arduino-esp32/issues/10370
+// 8) Discussion: https://github.com/espressif/arduino-esp32/issues/10370
 // 
 // pinMode() is a heavy machinery: setting a pin INPUT or OUTPUT dors not just
 // change pin modes: it also calls init/deinit functions of a driver associated 
