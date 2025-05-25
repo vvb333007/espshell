5fe2b4610ecc7c62267eb6ed5e7e881ee701c4ba
vvb333007 <vvb333007@gmail.com>
2025-05-26 00:15:07 +0700

    +	---

--- examples/standalone_shell/standalone_shell.ino
+++ examples/standalone_shell/standalone_shell.ino
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include "espshell.h"
 
+// Yes. It is as simple as that. Enjoy your shell in your Serial Monitor
+
 void setup() { Serial.begin(115200); }
 void loop() { delay(9999); } 
 
